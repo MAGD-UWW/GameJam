d9e16d146b85c695d38cbde7c961b9aeedb4eadd
FrostFyres <frostfyreseo@gmail.com>
2015-11-21 05:54:22 -0600

    Added Spacebar transition from Title Screen

---
+++ Assets/Scripts/titleScreenTransition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class titleScreenTransition : MonoBehaviour {
+
+	
+	void Update () {
+		if(Input.GetKeyDown ("space")){
+			Debug.Log ("Changing the Level");
+			//Application.LoadLevel ("LevelChange");
+		}
+	
+	}
+}
