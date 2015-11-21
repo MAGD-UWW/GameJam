cdc922e50cff20e44b5a047187f7f43158ed3caf
christianheide <heidecr23@uww.edu>
2015-11-21 01:42:56 -0600

    Added moving block demo and uncompleted moving block script

---
+++ Assets/Scripts/moveBlock.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBlock : MonoBehaviour {
+	public Vector3 xMove;
+	public float maxZ;
+	public float minZ;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movingBlock ();
+		xMove = Vector3.forward * Time.deltaTime;
+	}
+
+	void movingBlock(){
+		transform.Translate(xMove);
+		if (transform.position.z == maxZ) {
+			transform.Translate (-xMove);
+		}
+		if (transform.position.z == minZ){
+			transform.Translate (xMove);
+		}
+	}
+}
