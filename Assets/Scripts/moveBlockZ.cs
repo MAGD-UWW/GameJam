daf30d65a6723a1e5533983f9bf8340a4bb8de2e
christianheide <heidecr23@uww.edu>
2015-11-21 04:21:04 -0600

    Made Level 3 alive and moving
    Level 3 has moving parts, colliders, and tags, the whole shabang

---
+++ Assets/Scripts/moveBlockZ.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBlockZ : MonoBehaviour {
+	private bool dirZ = true;
+	//public float speed = 2.0f;
+	public float zMax;
+	public float zMin;
+	
+	// Update is called once per frame
+	void Update () {
+		if (dirZ == true) {
+			transform.Translate (Vector3.forward*Time.deltaTime);
+		} else {
+			transform.Translate (-Vector3.forward*Time.deltaTime);
+		}
+
+		if (transform.position.z >= zMax) {
+			dirZ = false;
+		}
+
+		if (transform.position.z <= zMin) {
+			dirZ = true;
+		}
+	}
+}
