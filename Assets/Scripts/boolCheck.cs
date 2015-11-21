a2b26a490de16615d10052568916d95e5217989b
christianheide <heidecr23@uww.edu>
2015-11-20 23:00:44 -0600

    Added checkpoints & boolCheck script
    These scripts are necessary to spawn the player to either the
    "startpoint" or the "checkpoint" if the player falls off the map. To
    spawn at the "checkpoint", the player needs to collide with the floating
    sphere, which is the checkpoint place holder

---
+++ Assets/Scripts/boolCheck.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boolCheck : MonoBehaviour {
+	public static bool checkpoint = false;
+
+	
+	// Update is called once per frame
+	void OnTriggerEnter () {
+		checkpoint = true;
+		Debug.Log ("checkpoint is active!");
+	}
+}
