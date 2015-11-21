bce816dd6fbdc5895dd545079377153667e222ee
FrostFyres <frostfyreseo@gmail.com>
2015-11-21 01:11:46 -0600

    UI Timer Test

--- Assets/Scripts/sceneTransition.cs
+++ Assets/Scripts/sceneTransition.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class sceneTransition : MonoBehaviour {
 	public float timeLimit;
+	public GameObject deathUI;
+	public Text timer;
 
 
 	void Start () {
-		
+		deathUI = GameObject.Find ("deathUI");
+		deathUI.SetActive (false);
 	}
 
 	void Update () {
-		//Death ();
+		timer.text = Mathf.Round(timeLimit).ToString ();
+		Death ();
 	}
 	void Death(){
+		deathUI.SetActive (true);
 		timeLimit -= Time.deltaTime;
 		Debug.Log (timeLimit);
 		if (timeLimit <= 0) {
+			timeLimit = 0;
 			//Application.LoadLevel ("gameOver");
 			Debug.Log ("game over");
 			//Set canvas to true
